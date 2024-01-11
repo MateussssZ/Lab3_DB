683161b7eadd96de1f45ef13f86bbfe15e4f7c4d
MateussssZ <124344884+MateussssZ@users.noreply.github.com>
2024-01-11 21:20:43 +0300

    Update Pandas for config

--- Pandas.py
+++ Pandas.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import time
 
+class PandasBench():
+    Num_Of_Tests=0 #Количество тестов
+    CSV_Folder='' #Путь к CSV файлам
+
+    def __init__(self,Num_Of_Tests,CSV_Folder): #Инициализация
+        self.Num_Of_Tests=Num_Of_Tests
+        self.CSV_Folder=CSV_Folder
+
 def first_Query(current_df, total_time):
     start=time.time()
     selected_df = current_df[["VendorID"]] #Выбираем нужные нам столбцы
@@ -35,36 +43,36 @@ def fourth_Query(current_df, total_time):
 
 def check_Time(mode,table_df,n): #mode - tiny или big, table_df - нужный dataframe для работы, n - количество тестов
     total_time=[0]*4 #Время на каждом из 4 запросов
-    for i in range(n):
-        first_Query(table_df,total_time)
-        second_Query(table_df,total_time)
-        third_Query(table_df,total_time)
-        fourth_Query(table_df,total_time)
-    if (mode=='big'):
-            big_total_time[0]=total_time[0]
-            big_total_time[1]=total_time[1]
-            big_total_time[2] = total_time[2]
-            big_total_time[3] = total_time[3]
+    if (mode=="tiny"):
+        for i in range(n):
+            first_Query(table_df,total_time)
+            second_Query(table_df,total_time)
+            third_Query(table_df,total_time)
+            fourth_Query(table_df,total_time)
     else:
-        print(f"Average working time of first query on {mode} dataset is {(total_time[0] / n):.03f}s\n"
-          f"Average working time of second query on {mode} dataset  is {(total_time[1] / n):.03f}s\n"
-          f"Average working time of third query on {mode} dataset  is {(total_time[2] / n):.03f}s\n"
-          f"Average working time of fourth query on {mode} dataset is {(total_time[3] / n):.03f}s\n")
+        for i in range(n):
+            for chunk in table_df:
+                first_Query(chunk, total_time)
+                second_Query(chunk, total_time)
+                third_Query(chunk, total_time)
+                fourth_Query(chunk, total_time)
+    print(f"[Pandas]  Average working time of first query on {mode} dataset is {(total_time[0] / n):.03f}s\n"
+          f"[Pandas]  Average working time of second query on {mode} dataset  is {(total_time[1] / n):.03f}s\n"
+          f"[Pandas]  Average working time of third query on {mode} dataset  is {(total_time[2] / n):.03f}s\n"
+          f"[Pandas]  Average working time of fourth query on {mode} dataset is {(total_time[3] / n):.03f}s\n")
 
-try:
-    pd.options.mode.chained_assignment = None  # default='warn' Убираем лишние warning, чтобы не мазолили глаза
-    tiny_table_df = pd.read_csv("./DBases/nyc_yellow_tiny.csv")
-    check_Time('tiny',tiny_table_df,15) #Считываем малый датасет и сразу отправляем на тесты
-    big_table_df = pd.read_csv("./DBases/nyc_yellow_big-001.csv",chunksize=10000000) #Большой датасет сжирает к **** мои 16 Гб оперативы
-    big_total_time=[0]*4
-    num_of_tests=10;
-    for chunk in big_table_df:  #И крашит процесс, поэтому делим его на 2 примерно равных чанка и замеряем для каждого чанка время по очереди
-        check_Time('big', chunk, num_of_tests)
-    print(f"Average working time of first query on big dataset is {(big_total_time[0] / num_of_tests):.03f}s\n"
-          f"Average working time of second query on big dataset  is {(big_total_time[1] / num_of_tests):.03f}s\n"
-          f"Average working time of third query on big dataset  is {(big_total_time[2] / num_of_tests):.03f}s\n"
-          f"Average working time of fourth query on big dataset is {(big_total_time[3] / num_of_tests):.03f}s\n")
-except Exception as error:  # При обнаружении ошибки выдаём ошибку
-    print("Ошибка при работе с Pandas:", error)
-finally: #При успешной работе завершаем её
-    print("Работа с Pandas завершена")
+def start(config):
+    try:
+        pd.options.mode.chained_assignment = None  # default='warn' Убираем лишние warning, чтобы не мозолили глаза
+        tiny_table_df = pd.read_csv(config.CSV_Folder+"/nyc_yellow_tiny.csv")
+        big_table_df = pd.read_csv(config.CSV_Folder+"/nyc_yellow_big-001.csv",
+                                   chunksize=10000000)  # Большой датасет сжирает к **** мои 16 Гб оперативы
+
+        Num_Of_Tests=config.Num_Of_Tests
+        check_Time('tiny',tiny_table_df,Num_Of_Tests) #Считываем малый датасет и сразу отправляем на тесты
+        check_Time("big",big_table_df,Num_Of_Tests)
+
+    except Exception as error:  # При обнаружении ошибки выдаём ошибку
+        print("Ошибка при работе с Pandas:", error)
+    finally: #При успешной работе завершаем её
+        print("Работа с Pandas завершена\n")
